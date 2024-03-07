62b7b5d986b71e8d2ecb2f8470f43fa0a68d12e0
Olaf Szymanski <olafszymanski@C02FT4TKMD6M.local>
2024-03-07 19:44:32 +0100

    Add GetAny to Storager

--- storage/memory.go
+++ storage/memory.go
@@ -1,5 +1,9 @@
 package storage
 
+import "fmt"
+
+var ErrNotFound = fmt.Errorf("key not found")
+
 type MemoryStorage struct {
 	storage map[string]any
 }
@@ -8,6 +12,14 @@ func NewMemory() Storager {
 	return &MemoryStorage{
 		storage: make(map[string]any),
 	}
+}
+
+func (m *MemoryStorage) GetAny(key string) (any, error) {
+	v, ok := m.storage[key]
+	if !ok {
+		return nil, ErrNotFound
+	}
+	return v, nil
 }
 
 func (m *MemoryStorage) StoreAny(key string, value any) error {
--- storage/storage.go
+++ storage/storage.go
@@ -1,5 +1,6 @@
 package storage
 
 type Storager interface {
+	GetAny(key string) (any, error)
 	StoreAny(key string, value any) error
 }
