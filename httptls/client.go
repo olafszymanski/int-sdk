3b4046b9000183fef1a9906d9ae607f8228ad639
Olaf Szymanski <olafszymanski@C02FT4TKMD6M.local>
2024-03-11 22:03:45 +0100

    Refactor http client

---
+++ httptls/client.go
@@ -0,0 +1,62 @@
+package httptls
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/Danny-Dasilva/CycleTLS/cycletls"
+)
+
+type Response struct {
+	Status    int
+	Body      []byte
+	Headers   map[string]string
+	TimeTaken time.Duration
+}
+
+type HTTPClient struct {
+	client *cycletls.CycleTLS
+}
+
+func NewHTTPClient() *HTTPClient {
+	c := cycletls.Init()
+	return &HTTPClient{
+		client: &c,
+	}
+}
+
+func (c *HTTPClient) Get(url string, options ...func(*cycletls.Options)) (*Response, error) {
+	return c.do(url, http.MethodGet, options...)
+}
+
+func (c *HTTPClient) Post(url string, options ...func(*cycletls.Options)) (*Response, error) {
+	return c.do(url, http.MethodPost, options...)
+}
+
+func (c *HTTPClient) do(url, method string, options ...func(*cycletls.Options)) (*Response, error) {
+	opts := cycletls.Options{}
+	for _, o := range options {
+		o(&opts)
+	}
+
+	t := time.Now()
+	res, err := c.client.Do(
+		url,
+		opts,
+		method,
+	)
+	tt := time.Since(t)
+
+	r := &Response{
+		Status:    res.Status,
+		Body:      []byte(res.Body),
+		Headers:   res.Headers,
+		TimeTaken: tt,
+	}
+
+	if err != nil {
+		return r, err
+	}
+
+	return r, nil
+}
--- httptls/options.go
+++ httptls/options.go
@@ -1,4 +1,4 @@
-package client
+package httptls
 
 import "github.com/Danny-Dasilva/CycleTLS/cycletls"
 
